aa602482a90655eae4c842c2cf595f8ebf5c0530
tolaa <mgamtaa@gmail.com>
2021-08-31 15:05:13 +0300

    checks whether lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if c is lowercase, 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	return (1);
+	return (0);
+}
