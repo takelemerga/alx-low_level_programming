7ef43a34f84bdbfeeaf02e18ecc6679a105ede12
tolaa <mgamtaa@gmail.com>
2021-08-31 15:22:16 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of n
+ * @n: integer to be tested
+ * Return: abs(n)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	return (-1*n);
+
+	return (n);
+}
