9bd1b3492d978ba83a2e3bfe912de83eb3d6b4d9
tolaa <mgamtaa@gmail.com>
2021-09-01 10:41:57 +0300

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+
+			if ((i * j) < 10)
+			{
+
+				_putchar(i * j + 48);
+					if (j != 9)
+					{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					}	
+
+			}
+
+			else
+			{
+			_putchar(' ');
+			_putchar(i * j / 10 + 48);
+			_putchar(i * j % 10 + 48);
+			
+			if (j != 9)
+			{
+			_putchar(',');
+			}
+			}
+		}
+	_putchar('\n');
+	}
+}
