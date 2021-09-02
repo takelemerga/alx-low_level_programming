d86841921b395cbc4b5ab08c3279d8ce7b696e57
tolaa <mgamtaa@gmail.com>
2021-09-02 14:33:06 +0300

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - checks if input is an uppercase letter in ASCII
  * @c: integer to check
@@ -7,7 +6,5 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	return (1);
-	return (0);
+	return (c >= 65 && c <= 90);
 }
