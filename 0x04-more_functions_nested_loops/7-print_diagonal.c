1b1646bb26e42955aeb2534d3b75b1a2dfe23081
tolaa <mgamtaa@gmail.com>
2021-09-02 14:39:12 +0300

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,19 +3,11 @@
  * print_diagonal - print a diagonal line of variable length
  * @n: length of line
  */
-int main(void)
-{
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
-}
 void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		int i; 
+		int i;
 
 		for (i = 0; i < n; i++)
 		{
