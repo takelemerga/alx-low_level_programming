a57190b98ec321f390108428dd7e0eb4e14dbb9a
tolaa <mgamtaa@gmail.com>
2021-09-02 15:27:00 +0300

    sign check

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -10,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
