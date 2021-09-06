592b2744ffdb3ef4994f5b7728d6415b05be8ecd
tolaa <mgamtaa@gmail.com>
2021-09-06 17:51:47 +0300

    character position

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: string s
+ */
+void rev_string(char *s)
+{
+	int size = 0;
+
+	int i = 0;
+
+	int k;
+
+	char temp;
+
+	while (*(s + i) != '\0')
+	{
+		size = size + 1;
+		i++;
+	}
+	for (k = 0; k < size / 2; k++)
+	{
+	temp = s[k];
+	s[k] = s[(size - 1) - k];
+	s[(size - 1) - k] = temp;
+	}
+	while (*s)
+	{
+	_putchar(*s);
+	s++;
+	}
+	_putchar('\n');
+}
+
