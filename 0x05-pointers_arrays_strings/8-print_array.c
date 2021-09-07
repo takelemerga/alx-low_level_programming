f7efc8067caaf7170ac5bf6d09c7e568c2ba5185
tolaa <mgamtaa@gmail.com>
2021-09-07 14:51:15 +0300

    array element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_array-prints n elments of array a
+ *@a:an integer array
+ *@n: number of elements to be printed
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+	printf("%d, ", *(a + i));
+	i++;
+	}
+printf("\n");
+}
