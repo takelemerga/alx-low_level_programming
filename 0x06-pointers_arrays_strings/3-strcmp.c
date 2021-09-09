3fa27ac55e773d071d88fc366bbc24fe05f3a416
tolaa <mgamtaa@gmail.com>
2021-09-09 16:02:46 +0300

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,18 +6,23 @@
  * @s2: string 2
  * Return: 1 if true, 0 if false
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int diff;
+	int i, diff;
 
-	if (*s != '\0')
-	{ 
-	diff = *s1 - *s2;
-	if (diff != 0) 
+	for (i = 0; s1[i] != '\0'; ++i)
 	{
-	diff = *s1 - *s2;
-	return (diff);
+		if (s1[i] > s2[i])
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
+		else if (s1[i] < s2[i])
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
 	}
-	return (diff);
-	}
+	return (0);
 }
