0bb4ddb91d39b949c5913e58917cb7a54df2c5df
tolaa <mgamtaa@gmail.com>
2021-09-09 12:03:01 +0300

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int diff;
+
+	diff = *s1 - *s2;
+	if (diff != 0)
+	{
+	diff = *s1 - *s2;
+	return (diff);
+	}
+	return (diff);
+}
