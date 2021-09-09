757d9d2f665c2ee19b8572845832696b93db9386
tolaa <mgamtaa@gmail.com>
2021-09-09 16:22:47 +0300

    concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,43 +1,30 @@
 #include "main.h"
-
 /**
- * _strlen - returns the length of a string
- * @c: c is pointer that holds character array base adress
- * Return: length of string
+ * _strncat - concatenates two strings
+ * @dest: first input string
+ * @src: second input string
+ * @n: number of butes to be used
+ *
+ * Description: use at most n bytes from src
+ * Return: returns concatenated string
  */
-int _strlen(char *c)
-{
-	int length = 0;
 
-	while (*c != '\0')
-	{
-	length += 1;
-	c++;
-
-	}
-	return (length);
-}
-/**
-* _strncat - returns the concatenated string pointer
-*@dest: dest holds base adress of dest array
-*@src: src holds base adress of src string
-*@n: n number of bytes
-*Return: pointer
-*/
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int len, i;
 
-	j = _strlen(dest);
-	k = _strlen(src);
 	i = 0;
-	while (i < n && src[i] != 0)
+	len = 0;
+	while (dest[len] != '\0')
 	{
-	dest[i + j] = src[i];
-	i++;
-	j++;
+		++len;
 	}
-	dest[j] = '\0';
+	while (src[i] != 0 && i < n)
+	{
+		dest[len] = src[i];
+		++i;
+		++len;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
-
