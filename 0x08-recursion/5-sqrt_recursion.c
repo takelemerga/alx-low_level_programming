785fc49cfa19456b4a9588a0ad55b95047e61b58
tolaa <mgamtaa@gmail.com>
2021-09-16 15:13:54 +0300

    program to claculate square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,6 @@
  * @j: an input value
  * @i: input value
  *
- * Description: determines the square root
  * Return: returns the input val
  */
 
@@ -26,8 +25,6 @@ int is_square_root(int i, int j)
  * _sqrt_recursion - returns square root
  * @n: input size
  *
- * Description: If n does not have a natural square root,
- * the function should return -1
  * Return: returns the natural square root of a number
  */
 
