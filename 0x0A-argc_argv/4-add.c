e8ee7ac0f2fe2ae303f00e03ce35bf4114768ffb
tolaa <mgamtaa@gmail.com>
2021-09-17 16:01:17 +0300

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - multiply numbers
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int i, sum, num;
+
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
