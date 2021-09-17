50e72bc22677c6a9a7c2b700bc53d1c472f99978
tolaa <mgamtaa@gmail.com>
2021-09-17 11:01:14 +0300

    add program that prints all arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints  all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argc[i]);
+		i++;
+	}
+	return (0);
+}
+
