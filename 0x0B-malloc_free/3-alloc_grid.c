6605707553c56e4cfd847cb64d707ef2cbe899a8
tolaa <mgamtaa@gmail.com>
2021-09-21 15:27:12 +0300

    two dime

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,14 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 	{
+		free(a);
 		return (NULL);
 	}
 	arr = (int **) malloc(height * sizeof(int *));
 
 	if (arr == 0)
 	{
+		free(a);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
