da24ed841dc7154a4a6e3231b38100a345b7ebb7
tolaa <mgamtaa@gmail.com>
2021-09-23 15:00:03 +0300

    header files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+unsigned int my_strlen(char *p);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
