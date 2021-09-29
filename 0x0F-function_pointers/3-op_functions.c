747da8e940c6914b4233b210a5b463311f5467d2
tolaa <mgamtaa@gmail.com>
2021-09-29 14:11:43 +0300

    a program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * get_op_func -  selects the correct function to
+ * perform the operation asked by the user
+ * @s: input string
+ * Return: pointer to a function
+*/
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
