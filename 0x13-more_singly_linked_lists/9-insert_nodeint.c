b75d84674eed9c7034aa8438a9b442cdc2d8cdfd
tolaa <mgamtaa@gmail.com>
2021-10-12 12:19:42 +0300

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
-    print_listint(head);
-    free_listint2(&head);
-    return (0);
-}
 /**
 * insert_nodeint_at_index - inserts a new node at a given position
 * @head: head of the list
@@ -54,11 +24,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		{
 			if (pos != 0)
 				return (NULL);
-			else
-			{
-				*head = temp;
-				return (temp);
-			}
+			*head = temp;
+			return (temp);
 		}
 		if (head != NULL && pos == 0)
 		{
